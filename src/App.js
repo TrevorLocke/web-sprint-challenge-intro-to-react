98aa4f12b8307942099eff747c5247d37a94c91a
Trevor Locke <locke.trevor@gmail.com>
2021-06-28 00:34:37 -0500

    done

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
-import React from 'react';
+import React, { useState, useEffect } from "react";
 import './App.css';
+import Character from './components/Character';
+import axios from "axios";
 
 const App = () => {
   // Try to think through what state you'll need for this app before starting. Then build out
@@ -9,11 +11,29 @@ const App = () => {
   // side effect in a component, you want to think about which state and/or props it should
   // sync up with, if any.
 
-  return (
-    <div className="App">
-      <h1 className="Header">Characters</h1>
-    </div>
-  );
-}
+  const [characters, setCharacters] = useState([]);
+
+
+  useEffect(() => {
+    axios
+      .get("https://swapi.dev/api/people/")
+      .then((response) => {
+        setCharacters(response.data);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }, []);
+		
+  
+    return (
+      <div className="App">
+        <h1 className="Header">Characters</h1>
+        {characters.map((char, i) => {
+          return <Character key={i} char={char} />;
+        })}
+      </div>
+    );
+      };
 
 export default App;
--- src/components/Character.js
+++ src/components/Character.js
@@ -1,1 +1,24 @@
 // Write your Character component here
+import React from "react";
+import styled from "styled-components";
+
+const Wrapper = styled.div`
+	background-color: #d3d3d3;
+	padding: 0.25em 2em;
+	margin: 2em auto;
+	width: 90%;
+	text-align: left;
+	border: 2px solid #e4a654;
+`;
+
+const Character = (props) => {
+	const { char } = props;
+
+	return (
+		<Wrapper>
+			<h1>{char.name}</h1>
+		</Wrapper>
+	);
+};
+
+export default Character;
